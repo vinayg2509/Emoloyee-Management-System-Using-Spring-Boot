98d84b7d13f557b6330891bc6fee7339e89d53ea
vinayg2509 <vinayg2509@gmail.com>
2026-05-08 15:31:03 +0530

    [Vinay]add EmployeeController with createEmployee endpoint

---
+++ src/main/java/com/ems/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.ems.controller;
+
+import com.ems.dto.EmployeeDto;
+import com.ems.services.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+@AllArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping("/create")
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto)
+    {
+        EmployeeDto savedEmployee=employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}

725c72ce08fad9f1462ecb021082c59aa5812966
vinayg2509 <vinayg2509@gmail.com>
2026-05-08 12:24:51 +0530

    [Vinay]: add EmployeeRepository extending JpaRepository for Employee CRUD operations

---
+++ src/main/java/com/ems/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.ems.repository;
+
+import com.ems.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}

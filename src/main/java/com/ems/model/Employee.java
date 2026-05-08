8165f817902014c49372fb6a3e71ea929179ee48
vinayg2509 <vinayg2509@gmail.com>
2026-05-08 12:11:13 +0530

    [Vinay]Added Employee entity with fields and given validiation for the fields

---
+++ src/main/java/com/ems/model/Employee.java
@@ -0,0 +1,36 @@
+package com.ems.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.Pattern;
+
+@Entity
+@Getter
+@Setter
+@Table(name="employees")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="emp_id")
+    private Long id;
+
+    @Column(name="first_name")
+    @Pattern(regexp = "^[a-zA-Z ]{2,50}$", message = "Invalid name")
+    private String fullName;
+
+    @Pattern(regexp = "^[a-zA-Z ]{2,50}$", message = "Invalid name")
+    @Column(name="last_name")
+    private String lastName;
+
+
+    @Column(name="email" ,nullable = false,unique = true)
+    @Pattern(regexp = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$")
+    private String email;
+}
